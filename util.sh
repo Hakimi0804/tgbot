d26cbee3ce4d3bc393c1cc9d187c35f681e42178
Firdaus Hakimi <hakimifirdaus944@gmail.com>
2022-02-18 05:14:35 +0800

    Initial commit
    * Create .gitignore
        -> Ignore .token.sh
    * Create util.sh
        -> Create tg() function
        -> Create update() function
    
    Signed-off-by: Firdaus Hakimi <hakimifirdaus944@gmail.com>

---
+++ util.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+source .token.sh
+
+API="https://api.telegram.org/bot$TOKEN"
+tg() {
+    case $1 in
+        --editmsg)
+            shift
+            local CHAT_ID=$1
+            local MSG_ID=$2
+            local NEW_TEXT=$3
+            curl -s "$API/editMessageText" -d "chat_id=$CHAT_ID" -d "message_id=$MESSAGE_ID" "text=$NEW_TEXT" | jq .
+            ;;
+        --sendmsg)
+            shift
+            local CHAT_ID=$1
+            local MSG=$2
+            local RESULT=$(curl -s "$API/sendMessage" -d "chat_id=$CHAT_ID" -d "text=$MSG")
+            SENT_MSG_ID=$(echo "$RESULT" | jq '.result | .message_id')
+            ;;
+        --delmsg)
+            shift
+            local CHAT_ID=$1
+            local MSG_ID=$2
+            curl -s "$API/deleteMessage" -d "chat_id=$CHAT_ID" -d "message_id=$MSG_ID" | jq .
+            ;;
+        --sendsticker)
+            shift
+            local CHAT_ID=$1
+            local FILE_ID=$2
+            curl "$API/sendSticker" -d "chat_id=$CHAT_ID" -d "sticker=$FILE_ID" | jq .
+            ;;
+    esac
+}
+
+update() {
+    alias echo='echo -n'
+    FETCH=$(curl -s "$API/getUpdates" | jq '.result[]')
+    if [ -n "$FETCH" ]; then
+        MSG_CONTENT=$(echo "$FETCH" | jq '.message | .text')
+        MSGGER=$(echo "$FETCH" | jq '.message | .from | .id')
+        RET_MSG_ID=$(echo "$FETCH" | jq '.message | .message_id')
+        RET_MSG_TEXT=$(echo "$FETCH" | jq '.message | .text')
+        RET_CHAT_ID=$(echo "$FETCH" | jq '.message | .chat | .id')
+        FIRST_NAME=$(echo "$FETCH" | jq '.message | .first_name')
+        USERNAME=$(echo "$FETCH" | jq '.message | .username')
+    fi
+}
