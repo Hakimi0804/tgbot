7ee71ad437d3b3caca51bed318b84678233a18b1
Firdaus Hakimi <hakimifirdaus944@gmail.com>
2022-02-22 07:39:26 +0800

    tgbot, extra: Cleanup
    Signed-off-by: Firdaus Hakimi <hakimifirdaus944@gmail.com>

--- extra.sh
+++ extra.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
 round() {
-    # e.g `round 2.3352 2`
-    #            ^~~~~^
-    # $1       Your number
-    #
-    #                   ^
-    # $2    The number of decimal places
+    # $1 = Your number
+    # $2 = Amount of decimal places
     FLOAT=$1
     DECIMAL_POINT=$2
     printf "%.${2:-$DECIMAL_POINT}f" "$FLOAT"
--- tgbot.sh
+++ tgbot.sh
@@ -11,39 +11,20 @@ while true; do
 
     echo "$RET_MSG_TEXT"
     case $RET_MSG_TEXT in
-        '/test'*)
-            tg --replymsg "$RET_CHAT_ID" "$RET_MSG_ID" "BOT is running"
-            ;;
-        '.help'*)
-            tg --replymsg "$RET_CHAT_ID" "$RET_MSG_ID" "No"
-            ;;
+        ## Not so useful
+        '/test'*) tg --replymsg "$RET_CHAT_ID" "$RET_MSG_ID" "BOT is running";;
+        '.help'*) tg --replymsg "$RET_CHAT_ID" "$RET_MSG_ID" "No";;
+
+        ## Useful utilities
         '.calc'*)
             TRIMMED="${RET_MSG_TEXT#.calc}"
             CALCED=$(echo "$TRIMMED" | bc -l 2>&1)
-            #echo "$TRIMMED $CALCED"
-            #od -c <<< "$CALCED"
             if ! echo "$CALCED" | grep -q 'syntax error'; then
                 ROUNDED=$(round "$CALCED" 3)
                 tg --replymsg "$RET_CHAT_ID" "$RET_MSG_ID" "$ROUNDED"
             else
                 tg --replymsg "$RET_CHAT_ID" "$RET_MSG_ID" "bruh, did you just entered nonsense, cuz bc ain't happy"
             fi
-            ;;
-        '.fwdpost')
-            PREV_POST=$(< "$HOME/.fwdpost_cooldown") || PREV_POST=$(( $(date +%s) - 11 ))
-            if [ "$(( $(date +%s) - PREV_POST ))" -le 10 ]; then
-                tg --replymsg "$RET_CHAT_ID" "$RET_MSG_ID" "Wait for 10 secs cooldown plox"
-                unset RET_MSG_TEXT
-                continue
-            fi
-            if [[ " ${FWD_APRROVED_CHAT_ID[*]} " =~ " $RET_CHAT_ID " ]]; then
-                tg --sendmsg "$RET_CHAT_ID" "Sending to @RM6785 ..."
-                tg --cpmsg "$RET_CHAT_ID" "$FWD_TO" "$RET_REPLIED_MSG_ID"
-                tg --editmsg "$RET_CHAT_ID" "$SENT_MSG_ID" "Post sent"
-            else
-                tg --replymsg "$RET_CHAT_ID" "$RET_MSG_ID" "You aren't allowed to use this command outside testing group"
-            fi
-            date +%s > "$HOME/.fwdpost_cooldown"
             ;;
         '.magisk'*)
             tg --sendmsg "$RET_CHAT_ID" "Fetching latest Magisk stable"
@@ -60,6 +41,26 @@ while true; do
             tg --editmarkdownv2msg "$RET_CHAT_ID" "$SENT_MSG_ID" "[Latest stable]($LATEST_STABLE)
 [Latest canary]($CANARY)"
             ;;
+
+        ## Prototypes
+        '.fwdpost')
+            PREV_POST=$(< "$HOME/.fwdpost_cooldown") || PREV_POST=$(( $(date +%s) - 11 ))
+            if [ "$(( $(date +%s) - PREV_POST ))" -le 10 ]; then
+                tg --replymsg "$RET_CHAT_ID" "$RET_MSG_ID" "Wait for 10 secs cooldown plox"
+                unset RET_MSG_TEXT
+                continue
+            fi
+            if [[ " ${FWD_APRROVED_CHAT_ID[*]} " =~ " $RET_CHAT_ID " ]]; then
+                tg --sendmsg "$RET_CHAT_ID" "Sending to @RM6785 ..."
+                tg --cpmsg "$RET_CHAT_ID" "$FWD_TO" "$RET_REPLIED_MSG_ID"
+                tg --editmsg "$RET_CHAT_ID" "$SENT_MSG_ID" "Post sent"
+            else
+                tg --replymsg "$RET_CHAT_ID" "$RET_MSG_ID" "You aren't allowed to use this command outside testing group"
+            fi
+            date +%s > "$HOME/.fwdpost_cooldown"
+            ;;
+
+        ## Restricted to bot owner
         '.save'*)
             if [ "$MSGGER" -ne "$BOT_OWNER_ID" ]; then
                 tg --replymsg "$RET_CHAT_ID" "$RET_MSG_ID" "Sir bro only the bot owner can use this"
@@ -74,14 +75,11 @@ while true; do
             tg --fwdmsg "$RET_CHAT_ID" "$SAVING_GROUP_ID" "$RET_REPLIED_MSG_ID"
             tg --editmsg "$RET_CHAT_ID" "$SENT_MSG_ID" "Message forwarded"
             ;;
-        *[sS]ex*)
-            tg --replysticker "$RET_CHAT_ID" "$RET_MSG_ID" "CAACAgUAAxkBAAED-qJiE3HljFCcMMJOY9e12JvDnvk7mAACCAgAAvNoIFQU9d93MQ1XZSME"
-            ;;
-        *t[ea]st[eu]r*mo[ra][er]*p*ro*than*dev*)
-            tg --replysticker "$RET_CHAT_ID" "$RET_MSG_ID" "CAACAgQAAxkBAAED9_FiEMXeRur9aLMvyNnkj02cZew2ggACpAEAAsIupRbTkf08grqV_SME"
-            ;;
+
+        ## Funs / Miscs
+        *[sS]ex*) tg --replysticker "$RET_CHAT_ID" "$RET_MSG_ID" "CAACAgUAAxkBAAED-qJiE3HljFCcMMJOY9e12JvDnvk7mAACCAgAAvNoIFQU9d93MQ1XZSME";;
+        *t[ea]st[eu]r*mo[ra][er]*p*ro*than*dev*) tg --replysticker "$RET_CHAT_ID" "$RET_MSG_ID" "CAACAgQAAxkBAAED9_FiEMXeRur9aLMvyNnkj02cZew2ggACpAEAAsIupRbTkf08grqV_SME";;
     esac
 
     unset RET_MSG_TEXT RET_REPLIED_MSG_ID
-#    sleep .2
 done
