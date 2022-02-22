2e714d304c76844fc3b4a16afb3b8fa8f43eb2fa
Firdaus Hakimi <hakimifirdaus944@gmail.com>
2022-02-22 22:45:04 +0800

    tgbot: reply to message instead
    Signed-off-by: Firdaus Hakimi <hakimifirdaus944@gmail.com>

--- tgbot.sh
+++ tgbot.sh
@@ -13,7 +13,12 @@ while true; do
     case $RET_MSG_TEXT in
         ## Not so useful
         '/test'*) tg --replymsg "$RET_CHAT_ID" "$RET_MSG_ID" "BOT is running";;
-        '.help'*) tg --replymsg "$RET_CHAT_ID" "$RET_MSG_ID" "No";;
+        '.help'*)
+            tg --replymarkdownv2msg "$RET_CHAT_ID" "$RET_MSG_ID" "\`.calc\` \\-\\> Do math calculations
+\`.magisk\` \\-\\> Get latest magisk stable and canary
+\`.fwdpost\` \\-\\> Forward post to @RM6785 \\(this command is restricted to testing group\\)
+\`.postupdatesticker\` \\-\\> Post update sticker to @RM6785 \\(this command is restricted to testing group\\)"
+            ;;
 
         ## Useful utilities
         '.calc'*)
@@ -51,7 +56,7 @@ while true; do
             elif [ "$RET_REPLIED_MSG_ID" = "null" ]; then
                 tg --replymsg "$RET_CHAT_ID" "$RET_MSG_ID" "Reply to a message to post plox"
             elif [[ " ${FWD_APRROVED_CHAT_ID[*]} " =~ " $RET_CHAT_ID " ]]; then
-                tg --sendmsg "$RET_CHAT_ID" "Sending to @RM6785 ..."
+                tg --replymsg "$RET_CHAT_ID" "$RET_MSG_ID" "Sending to @RM6785 ..."
                 tg --cpmsg "$RET_CHAT_ID" "$FWD_TO" "$RET_REPLIED_MSG_ID"
                 tg --editmsg "$RET_CHAT_ID" "$SENT_MSG_ID" "Post sent"
                 date +%s > "$HOME/.fwdpost_cooldown"
@@ -65,7 +70,7 @@ while true; do
                 tg --replymsg "$RET_CHAT_ID" "$RET_MSG_ID" "Wait for 10 secs cooldown plox"
                 unset RET_MSG_TEXT
             elif [[ " ${FWD_APRROVED_CHAT_ID[*]} " =~ " $RET_CHAT_ID " ]]; then
-                tg --sendmsg "$RET_CHAT_ID" "Sending to @RM6785 ..."
+                tg --replymsg "$RET_CHAT_ID" "$RET_MSG_ID" "Sending to @RM6785 ..."
                 tg --sendsticker "$FWD_TO" "$RM6785_UPDATE_STICKER"
                 tg --editmsg "$RET_CHAT_ID" "$SENT_MSG_ID" "Sticker sent"
                 date +%s > "$HOME/.stickerpost_cooldown"
