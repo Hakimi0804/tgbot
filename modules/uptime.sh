41e3011694328b43efa4b7818acbf1da3d8d7f56
Firdaus Hakimi <hakimifirdaus944@gmail.com>
2022-03-04 06:12:00 +0800

    Pass all script through shfmt
    Signed-off-by: Firdaus Hakimi <hakimifirdaus944@gmail.com>

--- modules/uptime.sh
+++ modules/uptime.sh
@@ -2,10 +2,9 @@
 
 uptime() {
 	case $RET_LOWERED_MSG_TEXT in
-    '.uptime'*)
-    	UPTIME_OUTPUT=$(command uptime -p)
+	'.uptime'*)
+		UPTIME_OUTPUT=$(command uptime -p)
 		tg --replymsg "$RET_CHAT_ID" "$RET_MSG_ID" "Uptime: $UPTIME_OUTPUT"
-        true
 		;;
 	esac
 }
