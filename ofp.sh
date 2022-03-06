592f9996e1224d3f468d5a47e5ba9cefe4add21f
noobyysauraj <noobyysauraj@users.noreply.github.com>
2022-03-07 04:16:21 +0800

    Add sauraj's ofp stuff
    Signed-off-by: Firdaus Hakimi <hakimifirdaus944@gmail.com>

---
+++ ofp.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+##Make sure you have gdown & xlsx2csv installed
+##Install by <pip install gdown && pip install xlsx2csv>
+
+#Download sheet which contains ofp links.
+gdown "$OFP_SHEET" # This var refers to a secret in .token.sh
+
+#convert to csv file
+xlsx2csv Software update summary form新版软件汇总表.xlsx > ofp.csv
+
+#Grep links from csv file
+touch temp.txt
+grep "${1}.*${2}" ofp.csv | grep ".${3}." | egrep -o "(http|https)://[a-zA-Z0-9./?=_%:-]*" >temp.txt
+
+#format output
+echo ""
+echo ""
+sed -i "s/\n/\n\n/" temp.txt
+text=$(cat temp.txt)
+echo $text
+
+#Delete files for next time
+rm *.csv
+rm *.*xlsx
+
+################
