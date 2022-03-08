ab4aab74b3bfddaa7a0d5d7f854aba4686a8ac7a
Firdaus Hakimi <hakimifirdaus944@gmail.com>
2022-03-08 14:25:34 +0800

    modules: realme_ofp: Get rid of SC1018 warning
    ^-- SC1018: This is a unicode non-breaking space. Delete and retype it.
    * The filename contains this unicode and not normal space, thus crucial for this usage.
    
    Signed-off-by: Firdaus Hakimi <hakimifirdaus944@gmail.com>

--- modules/realme_ofp.sh
+++ modules/realme_ofp.sh
@@ -28,7 +28,7 @@ _realme_ofp_handler() {
 		return 1
 	fi
 	gdown "$OFP_SHEET" # This var refers to a secret in .token.sh
-	xlsx2csv Software update summary form新版软件汇总表.xlsx > ofp.csv
+	xlsx2csv "Software update summary form新版软件汇总表.xlsx" > ofp.csv
 	touch temp.txt
 	grep "${1}.*${2}" ofp.csv | grep ".${3}." | egrep -o "(rms01|https).*[a-z]" >temp.txt
 	sed -i "s/\n/\n\n/" temp.txt
